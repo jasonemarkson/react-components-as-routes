f91d807d589c3842aff8ee070ed67367ad0d9b6c
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-16 11:34:24 -0400

    Done.

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return(
+            <h1>Home!</h1>
+        )
+    }
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import Navbar from './Navbar'
 
-const Home = () => {
+const Banner = () => {
   return (
+  <div>
+    <h1>This is a website banner</h1>
+  </div>
+  )
+}
+ 
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route path="/" component={Banner} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
